ed6a743256631a77bd89a3576916c99e72af833b
Andy Raines <andrew.raines@news.co.uk>
2018-08-01 17:07:17 +0100

    Deal with projects which do not currently have tagging/releases

--- slackbot.sh
+++ slackbot.sh
@@ -16,11 +16,19 @@ echo "PROJECT_NAME:      $PROJECT_NAME"
 echo "RELEASED_BY:       $RELEASED_BY"
 echo "SLACK_URL_RELEASE: $SLACK_URL_RELEASE"
 
-# Lookup the tag & release url from the git commit hash used
+# Lookup the tag from the git commit hash used
 tag=$(git ls-remote "$GIT_REPO_URL" | grep "$GIT_HASH" | grep 'refs/tags/' | sed 's|.*refs/tags/||')
-releaseUrl=$(echo "$GIT_REPO_URL" | sed 's|^.*@\(.*\):\(.*\).git$|https://\1/\2/releases/tag/|')
+
+# Create a URL to the tag (or commit if no tag)
+githubUrl=$(echo "$GIT_REPO_URL" | sed 's|^.*@\(.*\):\(.*\).git$|https://\1/\2|')
+if [ -z "$tag" ]; then
+    tag=$GIT_HASH
+    releaseUrl="$githubUrl/commit/$GIT_HASH"
+else
+    releaseUrl="$githubUrl/releases/tag/$tag"
+fi
 
 # Format and send slack webhook
 format='{"attachments":[{"author_name": "Released by: %s", "title": "%s", "text": "Version %s has been released\n%s"}]}'
-data=$(printf "$format" "$RELEASED_BY" "$PROJECT_NAME" "$tag" "$releaseUrl/$tag")
+data=$(printf "$format" "$RELEASED_BY" "$PROJECT_NAME" "$tag" "$releaseUrl")
 curl -X POST -H 'Content-type: application/json' --data "$data" "$SLACK_URL_RELEASE"
