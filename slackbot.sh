6f098beec082761184e251d10b38d0e9027c9aba
Berdusco <sberdusco@mc111309.ni.ad.newsint>
2018-10-04 16:22:10 +0100

    add release changelog to slack message

--- slackbot.sh
+++ slackbot.sh
@@ -4,6 +4,7 @@
 # Expects the following environment variables to be available:
 #  * GIT_REPO_URL      The git repository URL (e.g. git@github.com:newsuk/project.git)
 #  * GIT_HASH          The commit hash of the build, must be tagged!
+#  * GIT_TOKEN         The git token for git api
 #  * PROJECT_NAME      The project name
 #  * RELEASED_BY       The user who released
 #  * SLACK_URL_RELEASE The slack webhook url for the appropriate channel to post to
@@ -19,6 +20,11 @@ echo "SLACK_URL_RELEASE: $SLACK_URL_RELEASE"
 # Lookup the tag from the git commit hash used
 tag=$(git ls-remote "$GIT_REPO_URL" | grep "$GIT_HASH" | grep 'refs/tags/' | sed 's|.*refs/tags/||')
 
+release_id=$(curl https://api.github.com/repos/newsuk/times-public-api/releases/tags/$tag?access_token=$GIT_TOKEN | jq .id)
+
+# This also change git from pre-release to release
+changelog=$(curl --data '{"prerelease": false}' -X PATCH https://api.github.com/repos/newsuk/times-public-api/releases/$release_id?access_token=$GIT_TOKEN | jq .body | sed -e 's/"//g')
+
 # Create a URL to the tag (or commit if no tag)
 githubUrl=$(echo "$GIT_REPO_URL" | sed 's|^.*@\(.*\):\(.*\).git$|https://\1/\2|')
 if [ -z "$tag" ]; then
@@ -29,6 +35,7 @@ else
 fi
 
 # Format and send slack webhook
-format='{"attachments":[{"author_name": "Released by: %s", "title": "%s", "text": "Version %s has been released\n%s"}]}'
-data=$(printf "$format" "$RELEASED_BY" "$PROJECT_NAME" "$tag" "$releaseUrl")
+format='{"attachments":[{"author_name": "Released by: %s", "title": "%s", "text": "Version %s has been released\n%s\n\n%s"}]}'
+data=$(printf "$format" "$RELEASED_BY" "$PROJECT_NAME" "$tag" "$releaseUrl" "$changelog")
+
 curl -X POST -H 'Content-type: application/json' --data "$data" "$SLACK_URL_RELEASE"
